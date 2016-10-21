71d00d33713288e12ca8b2aaf11cd00474146761
Joona Verneri Kannisto <joonakannisto@dyn-178-252.public.tut.fi>
2016-10-21 13:20:26 +0300

    commands to build and to launch

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-sudo docker run -d -i --name freeradius    -p 1812:1812/udp    -p 1813:1813  -p 18120:18120 650f4a08b250
+sudo docker build -t testi/radius .
+sudo docker run -d -i --name freeradius    -p 1812:1812/udp    -p 1813:1813  -p 18120:18120 testi/radius 
